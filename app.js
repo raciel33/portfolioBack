8e5fa868f37bf7598190e3ee561bdffcabde571f
Raciel Reyes Blanco <racielreyes@hotmail.es>
2023-04-20 22:04:43 +0200

    subiendo portfolio

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const configMensaje = require('./configMensaje');
+const app = express();
+app.use(bodyParser.json());
+
+app.use(cors());
+
+app.post('/formulario', (req, res) => {
+    configMensaje(req.body);
+    res.status(200).send();
+})
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+//si no es ninguna de las rutas anteriores coje el index.html
+app.get('*', (req, resp) => {
+    resp.sendFile(path.resolve(__dirname, 'public/index.html'));
+});
+
+app.listen(3000, () => {
+    console.log('‘Servidor corriendo')
+});
